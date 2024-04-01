7730aed3fb3acc788b142af4083baa38a58521c7
hmhdigital <rashad@hmh.digital>
2024-03-31 23:29:18 -0400

    Moved header markup to partial

--- header.php
+++ header.php
@@ -45,15 +45,5 @@
 			<?php endif; ?>
 		</div><!-- .site-branding -->
 
-		<nav id="site-navigation" class="main-navigation">
-			<button class="menu-toggle" aria-controls="primary-menu" aria-expanded="false"><?php esc_html_e( 'Primary Menu', 'besu' ); ?></button>
-			<?php
-			wp_nav_menu(
-				array(
-					'theme_location' => 'menu-1',
-					'menu_id'        => 'primary-menu',
-				)
-			);
-			?>
-		</nav><!-- #site-navigation -->
+		<?php get_template_part('template-parts/header-menu')?>
 	</header><!-- #masthead -->
