b34d50bb9f7b5bb56f2c16d2750122b1e8c8e6be
hmhdigital <rashad@hmh.digital>
2024-03-31 23:28:28 -0400

    Setup for composer autoload, theme features, enqueue

--- functions.php
+++ functions.php
@@ -7,147 +7,28 @@
  * @package Bēsu
  */
 
-if ( ! defined( '_S_VERSION' ) ) {
+if ( ! defined( 'BESU_VERSION' ) ) {
 	// Replace the version number of the theme on each release.
-	define( '_S_VERSION', '1.0.0' );
+	define( 'BESU_VERSION', '1.0.0' );
+}
+
+ /**
+ * Navi composer autoload
+ */
+$composer_autoload = __DIR__ . '/vendor/autoload.php';
+if ( file_exists( $composer_autoload ) ) {
+	require_once $composer_autoload;
 }
 
 /**
- * Sets up theme defaults and registers support for various WordPress features.
- *
- * Note that this function is hooked into the after_setup_theme hook, which
- * runs before the init hook. The init hook is too late for some features, such
- * as indicating support for post thumbnails.
+ * Wordpress defaults and theme features.
  */
-function besu_setup() {
-	/*
-		* Make theme available for translation.
-		* Translations can be filed in the /languages/ directory.
-		* If you're building a theme based on Bēsu, use a find and replace
-		* to change 'besu' to the name of your theme in all the template files.
-		*/
-	load_theme_textdomain( 'besu', get_template_directory() . '/languages' );
-
-	// Add default posts and comments RSS feed links to head.
-	add_theme_support( 'automatic-feed-links' );
-
-	/*
-		* Let WordPress manage the document title.
-		* By adding theme support, we declare that this theme does not use a
-		* hard-coded <title> tag in the document head, and expect WordPress to
-		* provide it for us.
-		*/
-	add_theme_support( 'title-tag' );
-
-	/*
-		* Enable support for Post Thumbnails on posts and pages.
-		*
-		* @link https://developer.wordpress.org/themes/functionality/featured-images-post-thumbnails/
-		*/
-	add_theme_support( 'post-thumbnails' );
-
-	// This theme uses wp_nav_menu() in one location.
-	register_nav_menus(
-		array(
-			'menu-1' => esc_html__( 'Primary', 'besu' ),
-		)
-	);
-
-	/*
-		* Switch default core markup for search form, comment form, and comments
-		* to output valid HTML5.
-		*/
-	add_theme_support(
-		'html5',
-		array(
-			'search-form',
-			'comment-form',
-			'comment-list',
-			'gallery',
-			'caption',
-			'style',
-			'script',
-		)
-	);
-
-	// Set up the WordPress core custom background feature.
-	add_theme_support(
-		'custom-background',
-		apply_filters(
-			'besu_custom_background_args',
-			array(
-				'default-color' => 'ffffff',
-				'default-image' => '',
-			)
-		)
-	);
-
-	// Add theme support for selective refresh for widgets.
-	add_theme_support( 'customize-selective-refresh-widgets' );
-
-	/**
-	 * Add support for core custom logo.
-	 *
-	 * @link https://codex.wordpress.org/Theme_Logo
-	 */
-	add_theme_support(
-		'custom-logo',
-		array(
-			'height'      => 250,
-			'width'       => 250,
-			'flex-width'  => true,
-			'flex-height' => true,
-		)
-	);
-}
-add_action( 'after_setup_theme', 'besu_setup' );
+require get_template_directory() . '/inc/bootstrap.php';
 
 /**
- * Set the content width in pixels, based on the theme's design and stylesheet.
- *
- * Priority 0 to make it available to lower priority callbacks.
- *
- * @global int $content_width
+ *  Navi custom menu functionality.
  */
-function besu_content_width() {
-	$GLOBALS['content_width'] = apply_filters( 'besu_content_width', 640 );
-}
-add_action( 'after_setup_theme', 'besu_content_width', 0 );
-
-/**
- * Register widget area.
- *
- * @link https://developer.wordpress.org/themes/functionality/sidebars/#registering-a-sidebar
- */
-function besu_widgets_init() {
-	register_sidebar(
-		array(
-			'name'          => esc_html__( 'Sidebar', 'besu' ),
-			'id'            => 'sidebar-1',
-			'description'   => esc_html__( 'Add widgets here.', 'besu' ),
-			'before_widget' => '<section id="%1$s" class="widget %2$s">',
-			'after_widget'  => '</section>',
-			'before_title'  => '<h2 class="widget-title">',
-			'after_title'   => '</h2>',
-		)
-	);
-}
-add_action( 'widgets_init', 'besu_widgets_init' );
-
-/**
- * Enqueue scripts and styles.
- */
-function besu_scripts() {
-	wp_enqueue_style( 'besu-style', get_stylesheet_uri(), array(), _S_VERSION );
-	wp_style_add_data( 'besu-style', 'rtl', 'replace' );
-
-	wp_enqueue_script( 'besu-navigation', get_template_directory_uri() . '/js/navigation.js', array(), _S_VERSION, true );
-
-	if ( is_singular() && comments_open() && get_option( 'thread_comments' ) ) {
-		wp_enqueue_script( 'comment-reply' );
-	}
-}
-add_action( 'wp_enqueue_scripts', 'besu_scripts' );
+require get_template_directory() . '/inc/custom-menu.php';
 
 /**
  * Implement the Custom Header feature.
@@ -175,4 +56,30 @@ require get_template_directory() . '/inc/customizer.php';
 if ( defined( 'JETPACK__VERSION' ) ) {
 	require get_template_directory() . '/inc/jetpack.php';
 }
+
+/**
+ * Enqueue scripts and styles.
+ */
+function besu_scripts() {
+
+	 // Loads bundled theme CSS.
+	 wp_enqueue_style( 'besu-css-styles', get_template_directory_uri() . '/build/index.css',
+	 array(),
+	 (include get_template_directory() . '/build/index.asset.php')['version']
+	 );
+	 wp_style_add_data( 'besu-style', 'rtl', 'replace' );
+
+
+	// Loads bundled theme JS.
+	wp_enqueue_script('besu-js-scripts', get_template_directory_uri() . '/build/index.js',
+	(include get_template_directory() . '/build/index.asset.php')['dependencies'],
+	(include get_template_directory() . '/build/index.asset.php')['version'],
+	true
+	);
+
+	if ( is_singular() && comments_open() && get_option( 'thread_comments' ) ) {
+		wp_enqueue_script( 'comment-reply' );
+	}
+}
+add_action( 'wp_enqueue_scripts', 'besu_scripts' );
 
