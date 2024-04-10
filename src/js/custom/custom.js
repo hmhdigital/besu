9a6d7c097beb63f2e9d6c7d801ba371d35829565
hmhdigital <rashad@hmh.digital>
2024-04-10 00:02:14 -0400

    Fix: Load alpine after dom content loaded

--- src/js/custom/custom.js
+++ src/js/custom/custom.js
@@ -1,5 +1,8 @@
-import Alpine from "alpinejs";
+import Alpine from 'alpinejs';
 
-window.Alpine = Alpine
+window.Alpine = Alpine;
 
-Alpine.start()
+// Alpine loads at the bottom of body
+document.addEventListener( 'DOMContentLoaded', function( event ) {
+	Alpine.start();
+} );
